5ff34e5343c4192d17722f5a4efaa041fe8a8e76
a60231023 <ayushtiwari6061@gmail.com>
2023-04-29 21:10:23 +0530

    resume section updation

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,12 +4,8 @@ import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/AYUSH_TIWARI.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://drive.google.com/file/d/1JJqnzYmpdj4y_BY7fB1E3ONoiIQFMvOD/view?usp=share_link";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -34,11 +30,7 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
+        
       </Container>
     </div>
   );
